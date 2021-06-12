b6018fae8d71444980d4d3bb1651eaf433eb9c25
Shashank-Shastri <shashank.m.shastri@gmail.com>
2021-06-12 23:32:57 +0530

    Fix deploy workflow

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const app = createApp(App);
 app.config.globalProperties.$clsx = clsx;
 app.mount('#root');
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
-if (undefined /* [snowpack] import.meta.hot */) {
-    undefined /* [snowpack] import.meta.hot */
-        .accept();
-    undefined /* [snowpack] import.meta.hot */
-        .dispose(() => {
-            app.$destroy();
-        });
-}
+// // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+// if (undefined /* [snowpack] import.meta.hot */) {
+//     undefined /* [snowpack] import.meta.hot */
+//         .accept();
+//     undefined /* [snowpack] import.meta.hot */
+//         .dispose(() => {
+//             app.$destroy();
+//         });
+// }
