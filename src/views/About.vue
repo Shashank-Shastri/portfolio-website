06594e25daaf3fce6859474a1e34a4e6a2f39026
Shashank-Shastri <shashank.m.shastri@gmail.com>
2024-01-07 20:33:50 +0530

    Update resume

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,35 +5,27 @@
                 <div class="col-lg-10">
                     <h2>About me</h2>
                     <p class="lead mb-5">
-                        With a Bachelor's degree in Computer Engineering from
-                        the University of Mumbai and a passion for full-stack
-                        development, I have successfully created and maintained
-                        custom applications for various clients as a Retool
-                        Developer at
-                        <a
-                            class="text-light"
-                            target="_blank"
-                            href="https://www.linkedin.com/company/boldtech-dev"
-                            >Bold Tech</a
-                        >.
-                    </p>
-                    <p class="lead mb-5">
-                        Prior to joining
-                        <a
-                            class="text-light"
-                            target="_blank"
-                            href="https://www.linkedin.com/company/boldtech-dev"
-                            >Bold Tech</a
-                        >, I gained valuable experience in developing web
-                        applications, integrating APIs, conducting market
-                        research, and generating leads for various industries as
-                        a Full-Stack Engineer and a Market Analyst at
+                        As a seasoned Full Stack Developer with over 5 years and
+                        10 months of experience at
                         <a
                             class="text-light"
                             target="_blank"
                             href="https://www.linkedin.com/company/leadgenius"
                             >Leadgenius</a
-                        >.
+                        >, I thrive on transforming ideas into impactful
+                        technological solutions. My expertise lies in JavaScript
+                        and Vue.js for front-end development, coupled with a
+                        strong foundation in Python, Django, and Node.js for
+                        robust back-end solutions. I hold a Bachelor's degree in
+                        Computer Engineering from the University of Mumbai,
+                        where I cultivated the theoretical knowledge
+                        complementing my practical experience in the field. I am
+                        passionate about leveraging technology to solve
+                        real-world challenges and am open to collaboration and
+                        networking opportunities in the dynamic world of
+                        software development. Let's connect and explore how we
+                        can collaborate to drive innovation and efficiency in
+                        the tech space!
                     </p>
                 </div>
             </div>
